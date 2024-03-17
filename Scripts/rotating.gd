f496a26059c3ec3483171c09b4a89a5dac5647bd
Andrew Milburn <sidbell.069@gmail.com>
2024-03-17 16:34:26 +0000

    Update rotating.gd
    Giving Mr Robot a circular motion

--- Scripts/rotating.gd
+++ Scripts/rotating.gd
@@ -1,12 +1,11 @@
 extends Sprite2D
 
-var angular_speed = PI
+var speed = 400
 var rotation_speed = PI
-
+var velocity: Vector2 = Vector2(0,0)
 
 func _process(delta: float) -> void:
-	var velocity = Vector2.UP * 50
 	
 	rotation += rotation_speed * delta
-	
-	position += velocity.rotated(angular_speed) * delta
+	velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
