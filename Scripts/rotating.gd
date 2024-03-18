01469887cb5cb39e59e528969e83aafad4570954
Andrew Milburn <sidbell.069@gmail.com>
2024-03-18 08:54:52 +0000

    Video Finished

--- Scripts/rotating.gd
+++ Scripts/rotating.gd
@@ -1,10 +1,20 @@
 extends Sprite2D
 
-var speed = 400
+var speed = 200
 var rotation_speed = PI
 var velocity: Vector2 = Vector2(0,0)
 
 func _process(delta: float) -> void:
+	
+	if Input.is_action_pressed("ui_right"):
+		rotation_speed += 0.1
+	elif Input.is_action_pressed("ui_left"):
+		rotation_speed -= 0.1
+	
+	if Input.is_action_pressed("ui_up"):
+		speed += 10
+	elif Input.is_action_pressed("ui_down"):
+		speed -= 10
 	
 	rotation += rotation_speed * delta
 	velocity = Vector2.UP.rotated(rotation) * speed
