9dc32454acca61f3a4398bb81dbd1a64a97194b3
Andrew Milburn <sidbell.069@gmail.com>
2024-03-18 18:00:27 +0000

    Update rotating.gd
    Making it easier to control

--- Scripts/rotating.gd
+++ Scripts/rotating.gd
@@ -7,14 +7,14 @@ var velocity: Vector2 = Vector2(0,0)
 func _process(delta: float) -> void:
 	
 	if Input.is_action_pressed("ui_right"):
-		rotation_speed += 0.1
+		rotation_speed += 0.05
 	elif Input.is_action_pressed("ui_left"):
-		rotation_speed -= 0.1
+		rotation_speed -= 0.05
 	
 	if Input.is_action_pressed("ui_up"):
-		speed += 10
+		speed += 5
 	elif Input.is_action_pressed("ui_down"):
-		speed -= 10
+		speed -= 5
 	
 	rotation += rotation_speed * delta
 	velocity = Vector2.UP.rotated(rotation) * speed
